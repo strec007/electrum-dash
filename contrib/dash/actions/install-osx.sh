bfb2cfc04e9417548ed43f6ab153b9a17970b82f
pshenmic <pshenmic@gmail.com>
2023-12-19 18:02:07 +0400

    Unlink before running brew install

--- contrib/dash/actions/install-osx.sh
+++ contrib/dash/actions/install-osx.sh
@@ -40,7 +40,9 @@ rm -f libsecp256k1.0.dylib
 cp libsecp256k1/libsecp256k1.0.dylib .
 rm -rf libsecp256k1/ ${LSECP256K1_FILE} ${LSECP256K1_FILE}.sha256
 
-brew install gettext libtool automake pkg-config virtualenv --force
+brew link --overwrite python@3.12
+
+brew install gettext libtool automake pkg-config virtualenv
 
 echo "Building ZBar dylib..."
 rm -f libzbar.0.dylib
