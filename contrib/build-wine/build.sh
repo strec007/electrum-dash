997c4a80b3c7c18b1b56171aae07740322e28c4d
pshenmic <pshenmic@gmail.com>
2024-01-06 17:28:37 +0400

    Upgrade setuptools before run

--- contrib/build-wine/build.sh
+++ contrib/build-wine/build.sh
@@ -29,6 +29,7 @@ cp contrib/build-wine/deterministic.spec .
 cp contrib/dash/pyi_runtimehook.py .
 cp contrib/dash/pyi_tctl_runtimehook.py .
 
+wine python -m pip install --upgrade setuptools
 wine python -m pip install --no-dependencies --no-warn-script-location \
     -r contrib/deterministic-build/requirements.txt
 wine python -m pip install --no-dependencies --no-warn-script-location \
